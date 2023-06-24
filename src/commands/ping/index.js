64835e4dae1019ab183bf2c5371d578d9a481798
墨綠BG <s5460703@gmail.com>
2023-06-24 21:02:38 +0800

    [add] api&event

--- src/commands/ping/index.js
+++ src/commands/ping/index.js
@@ -1,9 +1,9 @@
 import { SlashCommandBuilder } from 'discord.js';
 
 export const command = new SlashCommandBuilder()
-    .setName('ping')
-    .setDescription('ping command');
+	.setName('ping')
+	.setDescription('ping command');
 
 export const action = async (ctx) => {
-    await ctx.reply('pong!');
+	ctx.reply('pong !');
 };
