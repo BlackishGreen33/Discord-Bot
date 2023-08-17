d49da830dc35e9972180316d199706e7d327b2ce
墨綠BG <s5460703@gmail.com>
2023-08-17 17:40:06 +0800

    Update index.js

--- src/events/retractMessageChoking/index.js
+++ src/events/retractMessageChoking/index.js
@@ -5,6 +5,6 @@ export const event = {
 };
 
 export const action = (msg) => {
-    msg.send(`${msg.author.username} 抓到  還敢偷刪啊`);
-    msg.send(`原訊息內容：${msg.content}`);
+    msg.channel.send(`${msg.author.username} 抓到  還敢偷刪啊`);
+    msg.channel.send(`原訊息內容：${msg.content}`);
 };
