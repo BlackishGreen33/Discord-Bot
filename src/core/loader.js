2e5c115976323bd2be4a69bda501d4653f54ecd8
2413 莊育誠 lionchuang <lionchuang@pershing.com.tw>
2023-08-21 10:40:17 +0800

    群組設置斜線命令復原，供原開發者使用

--- src/core/loader.js
+++ src/core/loader.js
@@ -5,9 +5,15 @@ import { useAppStore } from "@/store/app";
 const updateSlashCommands = async (commands) => {
 	const rest = new REST({ version: 10 }).setToken(process.env.TOKEN);
 	// delete guild id
-	await rest.put(Routes.applicationCommands(process.env.APPLICATION_ID), {
-		body: commands,
-	});
+	await rest.put(
+		Routes.applicationGuildCommands(
+			process.env.APPLICATION_ID,
+			"616449516840681476"
+		),
+		{
+			body: commands,
+		}
+	);
 };
 
 export const loadCommands = async () => {
